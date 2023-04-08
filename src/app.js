3b430e5bcf458d0eb1ca9b847dbbdb204a2b7603
leonardo-vic3nte <leonardo.vic3nte@gmail.com>
2023-04-08 02:06:51 -0300

    Add eventlisteners and functionality to digits, clear, clearall, decimalPoint

--- src/app.js
+++ src/app.js
@@ -6,21 +6,45 @@ const multiplication = (num1, num2) => num1 * num2;
 
 const division = (num1, num2) => num1 / num2;
 
-const operate = (num1, num2, operator) => {
-  switch (operator) {
-    case 'add':
-      return add(num1, num2);
-    case 'subtraction':
-      return subtraction(num1, num2);
-    case 'multiplication':
-      return multiplication(num1, num2);
-    case 'division':
-      return division(num1, num2);
-      default:
-        return null;
-  }
-};
+function operate(operator, firstOperand, secondOperand) {}
 
 function main() {
-  let firstOperand, operator, secondOperand;
+  const operationArea = document.querySelector('.operation-area');
+  const resultArea = document.querySelector('.result-area');
+
+  const digitButtons = document.querySelectorAll('.digits');
+  const decimalPoint = document.querySelector('.decimal');
+  const operationDigits = document.querySelectorAll('.operators');
+  const allClear = document.querySelector('.allclear');
+  const clear = document.querySelector('.clear');
+
+  allClear.addEventListener('click', () => {
+    operationArea.textContent = '0';
+    resultArea.textContent = '';
+  });
+
+  clear.addEventListener('click', () => {
+    if (operationArea.textContent[1] === '.') {
+      operationArea.textContent = operationArea.textContent.slice(1);
+    }
+    operationArea.textContent = operationArea.textContent.slice(1);
+  });
+
+  digitButtons.forEach(digitBtn => {
+    digitBtn.addEventListener('click', () => {
+      if (operationArea.textContent === '0') {
+        operationArea.textContent = digitBtn.value;
+      } else {
+        operationArea.textContent += digitBtn.value;
+      }
+    });
+  });
+
+  decimalPoint.addEventListener('click', () => {
+    if (!operationArea.textContent.includes('.') && operationArea.textContent) {
+      operationArea.textContent += decimalPoint.value;
+    }
+  });
 }
+
+main();
