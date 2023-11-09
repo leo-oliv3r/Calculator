54f06cda9576b441557b18302ff5d1577bb0a502
leonardo-vic3nte <leonardo.vic3nte@gmail.com>
2023-11-09 00:29:52 -0300

    Add some refactors

---
+++ src/calculator.ts
@@ -0,0 +1,61 @@
+type Operator = "+" | "-" | "*" | "/" | "%" | null;
+
+export class Calculator {
+    private firstOperand = "";
+    private secondOperand = "";
+    private operator: Operator = null;
+
+    private add(a: number, b: number) {
+        return a + b;
+    }
+
+    private substract(a: number, b: number) {
+        return a - b;
+    }
+
+    private multiply(a: number, b: number) {
+        return a * b;
+    }
+
+    private divide(a: number, b: number) {
+        if (b === 0) {
+            throw new Error("Cannot divide by 0");
+        }
+        return a / b;
+    }
+
+    private modulus(a: number, b: number) {
+        return a % b;
+    }
+
+    setFirstOperand(operand: string) {
+        this.firstOperand = operand;
+    }
+
+    setSecondOperand(operand: string) {
+        this.secondOperand = operand;
+    }
+
+    setOperator(newOperator: Operator) {
+        this.operator = newOperator;
+    }
+
+    operate(): number {
+        const n1 = Number(this.firstOperand);
+        const n2 = Number(this.secondOperand);
+        switch (this.operator) {
+            case "+":
+                return this.add(n1, n2);
+            case "-":
+                return this.substract(n1, n2);
+            case "*":
+                return this.multiply(n1, n2);
+            case "/":
+                return this.divide(n1, n2);
+            case "%":
+                return this.modulus(n1, n2);
+            default:
+                throw new Error("Something went wrong...");
+        }
+    }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,60 @@
-import { initDigitsListener } from "./ts/view";
+import { Calculator } from "./calculator";
 
-initDigitsListener();
+const lastOperation = document.querySelector(".calculator__last-operation") as HTMLParagraphElement;
+const currentOperation = document.querySelector(".calculator__current-operation") as HTMLParagraphElement;
+
+const digitsBtns = document.querySelectorAll(".digits");
+const operatorsBtns = document.querySelectorAll(".operators");
+const equalsBtn = document.querySelector(".equals") as HTMLButtonElement;
+const decimalPointBtn = document.querySelector(".decimal-point") as HTMLButtonElement;
+const clearBtn = document.querySelector(".clear-one") as HTMLButtonElement;
+const allClearBtn = document.querySelector(".allclear") as HTMLButtonElement;
+
+let clearScreenFlag = false;
+
+const calculator = new Calculator();
+
+function clearScreen() {
+    currentOperation.textContent = "";
+    clearScreenFlag = false;
+}
+
+function appendNumberToDisplay(numberText: string) {
+    if (currentOperation.textContent === "0" || clearScreen) {
+        clearScreen();
+    }
+
+    currentOperation.textContent += numberText;
+}
+
+function fullclear(params: type) {
+    lastOperation.textContent = "";
+    currentOperation.textContent = "0";
+    calculator.setFirstOperand("");
+    calculator.setSecondOperand("");
+    calculator.setOperator(null);
+}
+
+function addDecimalPoint() {
+    if (clearScreenFlag) clearScreen();
+    if (currentOperation.textContent === "") {
+        currentOperation.textContent = "0";
+    }
+    if (currentOperation.textContent?.includes(".")) return;
+
+    currentOperation.textContent += ".";
+}
+
+function initDigitsListener() {
+    const handleDigitClick = function (event: MouseEvent) {
+        const digitBtn = event.target as HTMLButtonElement;
+        currentOperation.textContent += digitBtn.textContent!;
+    };
+
+    digitsBtns.forEach((digitBtn) => {
+        const btnElement = digitBtn as HTMLButtonElement;
+        btnElement.addEventListener("click", handleDigitClick);
+    });
+}
+
+export { initDigitsListener };
